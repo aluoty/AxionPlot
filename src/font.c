5580235cb844bd080b9d6062c2b4fd9159f801a2
aluoty <alexanderluo0910@gmail.com>
2026-06-23 15:43:41 +0800

    fix font for web

--- src/font.c
+++ src/font.c
@@ -1,5 +1,7 @@
 #include "font.h"
 
+#include "font_data.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,46 +9,49 @@
 static Font g_font = {0};
 static bool g_loaded = false;
 
-static const char *FontSearchPaths(void) {
-    const char *env = getenv("AXION_FONT");
-    if (env && env[0]) return env;
-
-    static const char *paths[] = {
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-        "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
-        "/usr/share/fonts/truetype/DejaVuSans.ttf",
-        "/usr/share/fonts/TTF/DejaVuSans.ttf",
-        "/usr/share/fonts/truetype/msttcorefonts/Arial.ttf",
-        NULL
-    };
-
-    for (int i = 0; paths[i]; i++) {
-        FILE *f = fopen(paths[i], "rb");
-        if (f) {
-            fclose(f);
-            return paths[i];
-        }
-    }
-
-    return NULL;
-}
-
 Font UiLoadFont(int screen_height) {
     if (g_loaded) return g_font;
 
     int size = UiFontSize(screen_height);
-    const char *path = FontSearchPaths();
+    bool loaded = false;
 
-    if (path) {
-        g_font = LoadFontEx(path, size, 0, 0);
-        if (g_font.texture.id > 0) {
-            SetTextureFilter(g_font.texture, TEXTURE_FILTER_BILINEAR);
-            g_loaded = true;
-            return g_font;
-        }
+#if !defined(__EMSCRIPTEN__)
+    const char *env = getenv("AXION_FONT");
+    if (env && env[0]) {
+        g_font = LoadFontEx(env, size, 0, 0);
+        if (g_font.texture.id > 0) loaded = true;
     }
 
-    g_font = GetFontDefault();
+    if (!loaded) {
+        static const char *paths[] = {
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
+            "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
+            "/usr/share/fonts/truetype/DejaVuSans.ttf",
+            "/usr/share/fonts/TTF/DejaVuSans.ttf",
+            NULL
+        };
+        for (int i = 0; paths[i]; i++) {
+            g_font = LoadFontEx(paths[i], size, 0, 0);
+            if (g_font.texture.id > 0) { loaded = true; break; }
+        }
+    }
+#endif
+
+    if (!loaded) {
+        g_font = LoadFontFromMemory(
+            ".ttf",
+            _nix_store_37c8di1dc9zp4xfb1pzqdg1gbpbkniw5_dejavu_fonts_2_37_share_fonts_truetype_DejaVuSans_ttf,
+            (int)_nix_store_37c8di1dc9zp4xfb1pzqdg1gbpbkniw5_dejavu_fonts_2_37_share_fonts_truetype_DejaVuSans_ttf_len,
+            size, 0, 0
+        );
+    }
+
+    if (g_font.texture.id > 0) {
+        SetTextureFilter(g_font.texture, TEXTURE_FILTER_BILINEAR);
+    } else {
+        g_font = GetFontDefault();
+    }
+
     g_loaded = true;
     return g_font;
 }
